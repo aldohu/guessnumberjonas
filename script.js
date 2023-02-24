858ba2d02c1e9846231fd55a646be8d32edc9f93
Aldin <alhuric89@gmail.com>
2023-02-24 11:08:27 +0100

    add game logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'correct number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess'));
+
+
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Your guess is correct';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'You guess to Low';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'You guess to High';
+  }
+});
